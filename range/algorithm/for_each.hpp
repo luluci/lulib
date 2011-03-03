92562e754ef7338e7d785c73a136ac9d8754bf04
luluci <loci.lucis@gmail.com>
2011-03-03 11:37:20 +0900

    fix lulib/range

--- range/algorithm/for_each.hpp
+++ range/algorithm/for_each.hpp
@@ -2,9 +2,10 @@
 
 #include <lulib/range/detail/is_post_range.hpp>
 
+#include <boost/range/algorithm/for_each.hpp>
 #include <boost/utility/enable_if.hpp>
 
-#include <iostream>
+//#include <iostream>
 
 namespace lulib {
 	namespace range {
@@ -13,7 +14,7 @@ namespace lulib {
 		inline
 		typename boost::enable_if< detail::is_post_range<Range>, UnaryFunction >::type
 		for_each(Range &rng, UnaryFunction const& fun) {
-			std::cout << "post_range &" << std::endl;
+			//std::cout << "post_range &" << std::endl;
 			auto it = boost::begin(rng), end = boost::end(rng);
 			while (it != end) {
 				it.apply(fun);
@@ -25,7 +26,7 @@ namespace lulib {
 		inline
 		typename boost::enable_if< detail::is_post_range<Range>, UnaryFunction >::type
 		for_each(Range const& rng, UnaryFunction const& fun) {
-			std::cout << "post_range const&" << std::endl;
+			//std::cout << "post_range const&" << std::endl;
 			auto it = boost::begin(rng), end = boost::end(rng);
 			while (it != end) {
 				it.apply(fun);
@@ -38,14 +39,16 @@ namespace lulib {
 		inline
 		typename boost::disable_if< detail::is_post_range<Range>, UnaryFunction >::type
 		for_each(Range &rng, UnaryFunction const& fun) {
-			std::cout << "range &" << std::endl;
+			//std::cout << "range &" << std::endl;
+			boost::for_each(rng, fun);
 			return fun;
 		}
 		template<typename Range, typename UnaryFunction>
 		inline
 		typename boost::disable_if< detail::is_post_range<Range>, UnaryFunction >::type
 		for_each(Range const& rng, UnaryFunction const& fun) {
-			std::cout << "range &" << std::endl;
+			//std::cout << "range &" << std::endl;
+			boost::for_each(rng, fun);
 			return fun;
 		}
 
