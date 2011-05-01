52d1183d9a9b6bd41a0fd89e8cdbf6f9e25f73fc
luluci <loci.lucis@gmail.com>
2011-05-01 15:48:58 +0900

    不要ファイルを削除

--- lulib/network/protocol/make_request/header_container.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace lulib { namespace network { namespace protocol { namespace make_request {
-
-	typedef std::map<std::string,std::string> header_container;
-
-}}}}// namespace lulib::network::protocol::make_request
