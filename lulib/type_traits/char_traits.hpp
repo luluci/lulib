788e627a43b1670dc8d42f0542ea0bb5f0f6a6b3
luluci <lucis.cis@gmail.com>
2011-12-11 17:31:38 +0900

    fix lulib/type_traits/char_traits

--- lulib/type_traits/char_traits.hpp
+++ lulib/type_traits/char_traits.hpp
@@ -20,7 +20,9 @@ namespace lulib {
 
 		typedef enable_condition<
 			is_instance_of_basic_string<lulib::_1>,
-			is_nesting_value_type<lulib::_1>
+			is_nesting_value_type<lulib::_1>,
+			std::is_array<lulib::_1>,
+			std::is_pointer<lulib::_1>
 		> enable;
 	}// namespace lulib::char_traits_detail
 
@@ -39,6 +41,26 @@ namespace lulib {
 	template<typename T>
 	struct char_traits<T, typename char_traits_detail::enable::else_if_<1,T>::type> {
 		typedef typename T::value_type       char_type;
+		typedef std::basic_string<char_type> string_type;
+	};
+
+	// 配列
+	template<typename T>
+	struct char_traits<T, typename char_traits_detail::enable::else_if_<2,T>::type> {
+		typedef typename std::remove_pointer<
+			typename std::remove_cv<
+				typename std::decay<T>::type
+			>::type
+		>::type char_type;
+		typedef std::basic_string<char_type> string_type;
+	};
+
+	// ポインタ
+	template<typename T>
+	struct char_traits<T, typename char_traits_detail::enable::else_if_<3,T>::type> {
+		typedef typename std::remove_pointer<
+			typename std::remove_cv<T>::type
+		>::type char_type;
 		typedef std::basic_string<char_type> string_type;
 	};
 
